4fa9fa2b4806ec74b9b5bae678d97e869f58d717
Aleksandr <devtop000@gmail.com>
2018-11-16 20:06:31 +0200

    Products model updated

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,6 @@
 class Product < ApplicationRecord
+  validates :name, presence: true, length: { minimum: 3, maximun: 50},
+            uniqueness: { case_sensitive: false }
+  validates :description, length: {mininmum: 10, maximum: 255 }
+  validates :sale_price, length: {minimum: 1, maximum: 7}
 end
