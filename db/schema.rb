67c0712d92a928b3e14b1e336ff18f8dd6b2c8de
Aleksandr <devtop000@gmail.com>
2018-11-17 08:56:28 +0200

    Finished user edit, update, index, destroy

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_17_060129) do
+ActiveRecord::Schema.define(version: 2018_11_17_064843) do
 
   create_table "products", force: :cascade do |t|
     t.string "name"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2018_11_17_060129) do
     t.datetime "updated_at", null: false
     t.string "password_digest"
     t.string "remember_digest"
+    t.boolean "admin", default: false
   end
 
 end
