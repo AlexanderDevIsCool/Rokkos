a246318aafab5e06f2b7c0debb8ebd3108f7f96f
Aleksandr <devtop000@gmail.com>
2018-11-24 14:23:47 +0200

    schema and model

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,8 @@ class Product < ApplicationRecord
   validates :description, length: {mininmum: 10, maximum: 255 }
   validates :sale_price, length: {minimum: 1, maximum: 7}
 
+  has_many :desires
+  has_many :physicians, :through => :desires
   mount_uploader :image, ImageUploader
 
   validates :image, file_size: { less_than: 10.megabytes}
