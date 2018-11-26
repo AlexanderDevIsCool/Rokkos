0b97e07fca637347ac9bdd55838737e97c535090
Aleksandr <devtop000@gmail.com>
2018-11-26 18:51:38 +0200

    config mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -38,6 +38,14 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
   # config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for NGINX
 
+  config.action_mailer.smtp_settings = {
+      address:              'smtp.gmail.com',
+      port:                  587,
+      user_name:            'diskplaceorg@gmail.com',
+      password:             'ygnkgqmlgeajmeiv',
+      authentication:        :plain,
+      enable_starttls_auto: true
+  }
   # Store uploaded files on the local file system (see config/storage.yml for options)
   config.active_storage.service = :local
 
