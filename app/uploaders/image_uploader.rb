8bd194ac82d496bef905a6fe306f490e485cff2c
Aleksandr <devtop000@gmail.com>
2018-11-29 11:17:04 +0200

    save

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -35,9 +35,9 @@ class ImageUploader < CarrierWave::Uploader::Base
      process resize_to_fill: [350, 350]
    end
 
-  version :small_thumn, from_version: :thumb do
-    process resize_to_fill: [20, 20]
-  end
+ # version :small_thumn, from_version: :thumb do
+ #   process resize_to_fill: [20, 20]
+ # end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
